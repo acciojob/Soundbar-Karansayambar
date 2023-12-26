cbf1acb8cacd9080d74c1ef4d4fa2ccb98098f92
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2023-12-26 10:13:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function playSound(sound) {
+    const audio = new Audio(`sounds/${sound}`);
+    audio.play();
+}
+
+function playMusic(music) {
+    const audio = new Audio(`sounds/${music}`);
+    audio.play();
+}
+
+function stopAllSounds() {
+    const allSounds = document.querySelectorAll('audio');
+    allSounds.forEach(sound => sound.pause());
+}
