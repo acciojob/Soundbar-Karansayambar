33257609403e17277bb23f6b7b7c3435a7eaa518
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2023-12-26 15:47:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-//your JS code here. If required.
-document.querySelectorAll('.btn').forEach(button => {
-    button.addEventListener('click', () => {
-        let sound = new Audio('sounds/' + button.innerText + '.mp3');
-        sound.play();
-    });
-});
 
-document.querySelector('.stop').addEventListener('click', () => {
-});
